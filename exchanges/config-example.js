a72883e50ce9def6f6c4e02bd73c188f130a4b59
Serhii <kuts0882@gmail.com>
2024-11-16 12:49:58 +0200

    Update config.js

--- exchanges/config-example.js
+++ exchanges/config-example.js
@@ -1,7 +1,7 @@
 module.exports = {
     POLONIEX : {
-        API_KEY: 'X',
-        API_SECRET: 'X',
+        API_KEY: 'IM9SG4SY-0RGWAZ76-M547FYB4-BVW6A6M7',
+        API_SECRET: '830445238d5f0d77c46720c02b2993152ac6e0483e2439c5f81747e4700f77bd099a2deac4e1cce7b9006690afac57d8ee3a715e622d338d8ed770f0a52d5878',
         COINS: {
             BTCUSD:'BTC_USDT',
             LTCBTC:'BTC_LTC',
@@ -12,8 +12,8 @@ module.exports = {
         }
     },
     BITFINEX : {
-        API_KEY: 'X',
-        API_SECRET: 'X',
+        API_KEY: 'ec5388a19f0d24200d01d0160953bf49cd2256787d4',
+        API_SECRET: 'a4e854a4d433b2547c467d887bdffd35fb1c3e9b22f',
         COINS: {
             LTCBTC:'LTCBTC',
             ETHBTC:'ETHBTC',
